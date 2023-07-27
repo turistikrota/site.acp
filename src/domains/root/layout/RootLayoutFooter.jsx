e8e11807ccd5e97e721fa0b0c07478234007a687
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-07-28 00:27:38 +0300

    root: add layout footer

--- src/domains/root/layout/RootLayoutFooter.jsx
+++ src/domains/root/layout/RootLayoutFooter.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
 
 export default function RootLayoutFooter() {
-  return <></>;
+  return (
+    <footer className="footer">
+      <Container fluid={true}>
+        <Row>
+          <Col md={6}>{new Date().getFullYear()} © Turistikrota.</Col>
+          <Col md={6}>
+            <div className="text-sm-end d-none d-sm-block">
+              Tüm hakları saklıdır.
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
