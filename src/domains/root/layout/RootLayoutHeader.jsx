db86654d730e3b4f7340e58cc1dbfcab16c307c3
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-07-28 10:49:32 +0300

    place: add header layout for first time

--- src/domains/root/layout/RootLayoutHeader.jsx
+++ src/domains/root/layout/RootLayoutHeader.jsx
@@ -1,5 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "@/components/Logo/Logo";
+import RootLayoutNotificationDropdown from "./RootLayoutNotificationDropdown";
+import RootLayoutProfileDropdown from "./RootLayoutProfileDropdown";
 
 export default function RootLayoutHeader() {
-  return <></>;
+  return (
+    <header id="page-topbar">
+      <div className="navbar-header">
+        <div className="d-flex">
+          <div className="navbar-brand-box d-lg-none d-md-block">
+            <Link to="/" className="logo">
+              <Logo />
+            </Link>
+          </div>
+
+          <button
+            type="button"
+            onClick={() => {
+              console.log("togggggglee");
+            }}
+            className="btn btn-sm px-3 font-size-16 header-item "
+            id="vertical-menu-btn"
+          >
+            <i className="fa fa-fw fa-bars" />
+          </button>
+        </div>
+        <div className="d-flex">
+          <RootLayoutNotificationDropdown />
+          <RootLayoutProfileDropdown />
+        </div>
+      </div>
+    </header>
+  );
 }
