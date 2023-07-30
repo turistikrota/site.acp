ae9ad4e47894f2ca9fc9e8aeae90f46cd4d62125
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-07-30 17:02:35 +0300

    add image group lightbox

--- src/components/Kit/ImageUploader.jsx
+++ src/components/Kit/ImageUploader.jsx
@@ -1,24 +1,80 @@
 import { Services, apiUrl } from "@/config/service";
 import { httpClient } from "@/http/client";
 import { toFormData } from "@turistikrota/ui/utils/transform";
+import { useState } from "react";
 import Dropzone from "react-dropzone";
 import { useTranslation } from "react-i18next";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
 import { Button, Card } from "reactstrap";
 
-function ImagePreview({ files, onRemove }) {
-  const { t } = useTranslation("dropzone");
+function ImageGroupLightbox({
+  open = false,
+  index = 0,
+  setIndex,
+  images,
+  onClose,
+}) {
   return (
-    <div className="dropzone-previews mt-3 dz-processing dz-image-preview dz-success dz-complete">
-      {files.map((f, i) => (
-        <Card className="border dropzone-preview" key={i + "-file"}>
-          <img data-dz-thumbnail="" className="rounded" src={f} />
-          <Button color="danger" onClick={() => onRemove(f)}>
-            <i className="bx bx-xs bx-trash"></i>
-            {t("remove")}
-          </Button>
-        </Card>
-      ))}
-    </div>
+    <>
+      {open ? (
+        <Lightbox
+          mainSrc={images[index]}
+          nextSrc={images[(index + 1) % images.length]}
+          prevSrc={images[(index + images.length - 1) % images.length]}
+          enableZoom={true}
+          onCloseRequest={() => {
+            onClose();
+          }}
+          onMovePrevRequest={() => {
+            setIndex((index + images.length - 1) % images.length);
+          }}
+          onMoveNextRequest={() => {
+            setIndex((index + 1) % images.length);
+          }}
+          imageCaption={"Project " + parseFloat(index + 1)}
+        />
+      ) : null}
+    </>
+  );
+}
+
+function ImagePreview({ files, onRemove }) {
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewIndex, setPreviewIndex] = useState(0);
+  const { t } = useTranslation("dropzone");
+
+  const openPreview = (index) => {
+    setPreviewIndex(index);
+    setPreviewOpen(true);
+  };
+
+  return (
+    <>
+      <ImageGroupLightbox
+        open={previewOpen}
+        index={previewIndex}
+        images={files}
+        onClose={() => setPreviewOpen(false)}
+        setIndex={(index) => setPreviewIndex(index)}
+      />
+      <div className="dropzone-previews mt-3 dz-processing dz-image-preview dz-success dz-complete">
+        {files.map((f, i) => (
+          <Card className="border dropzone-preview" key={i + "-file"}>
+            <img
+              data-dz-thumbnail=""
+              className="rounded"
+              src={f}
+              onClick={() => openPreview(i)}
+            />
+            <Button color="danger" onClick={() => onRemove(f)}>
+              <i className="bx bx-xs bx-trash"></i>
+              {t("remove")}
+            </Button>
+          </Card>
+        ))}
+      </div>
+    </>
   );
 }
 
