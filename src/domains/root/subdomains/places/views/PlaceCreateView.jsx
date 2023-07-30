b4f58843163a2b1b606795d1a1bb7d34b700d8fa
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-07-30 23:18:54 +0300

    add feature uuids selection

--- src/domains/root/subdomains/places/views/PlaceCreateView.jsx
+++ src/domains/root/subdomains/places/views/PlaceCreateView.jsx
@@ -1,9 +1,12 @@
 import CardHeadContent from "@/components/Kit/CardHeadContent";
 import ImageUploader from "@/components/Kit/ImageUploader";
+import InputGroup from "@/components/Kit/InputGroup";
 import MarkdownEditor from "@/components/Kit/MarkdownContent";
 import RBreadcrumb from "@/components/Kit/RBreadcrumb";
 import { Config } from "@/config/config";
 import { Roles } from "@/config/roles";
+import { Services, apiUrl } from "@/config/service";
+import { useQuery } from "@/hooks/query";
 import { useMeta } from "@/utils/site";
 import { useFormik } from "formik";
 import { useState } from "react";
@@ -20,24 +23,31 @@ import {
   Label,
   Row,
 } from "reactstrap";
+import Spin from "sspin";
 import PageContentLayout from "~domains/root/layout/PageContentLayout";
 import ClaimGuardLayout from "~subdomains/account/layout/ClaimGuardLayout";
 
 const PlaceCreateView = () => {
+  const { data: features, isLoading } = useQuery(
+    apiUrl(Services.Place, "/feature"),
+    {
+      cache: true,
+    }
+  );
   const [trMarkdown, setTrMarkdown] = useState("");
   const [enMarkdown, setEnMarkdown] = useState("");
   const [images, setImages] = useState([
     "https://pbs.twimg.com/media/F2R-vxbXoAANyZz?format=webp&name=900x900",
     "https://pbs.twimg.com/media/F2RiK6cXoAAKgUY?format=webp&name=900x900",
   ]);
-  const { t } = useTranslation("places");
+  const { t, i18n } = useTranslation("places");
   useMeta(t("create.title"));
 
   const form = useFormik({
     initialValues: {
       featureUUIDs: [],
       images: [],
-      coordinates: [],
+      coordinates: [0, 0],
       timeSpent: {
         min: 0,
         max: 0,
@@ -98,6 +108,32 @@ const PlaceCreateView = () => {
                 </CardHeader>
                 <CardBody>
                   <Row>
+                    <Spin loading={isLoading}>
+                      <Col xs="12">
+                        <InputGroup
+                          label={t("form.basic.featureUUIDs.label")}
+                          name="featureUUIDs"
+                          error={form.errors.featureUUIDs}
+                        >
+                          <Select
+                            classNamePrefix="select2-selection"
+                            placeholder={t(
+                              "form.basic.featureUUIDs.placeholder"
+                            )}
+                            title={t("form.basic.featureUUIDs.title")}
+                            options={
+                              features
+                                ? features.map((d) => ({
+                                    value: d.uuid,
+                                    label: d.translations[i18n.language].title,
+                                  }))
+                                : []
+                            }
+                            isMulti
+                          />
+                        </InputGroup>
+                      </Col>
+                    </Spin>
                     <Col sm="6">
                       <div className="mb-3">
                         <Label htmlFor="productname">Product Name</Label>
@@ -186,18 +222,59 @@ const PlaceCreateView = () => {
               </Card>
             </Col>
             <Col xs="12">
-              <Card>
+              <Card className="r-card">
                 <CardHeader>
                   <CardHeadContent
                     title={t("form.coordinates.title")}
                     subtitle={t("form.coordinates.subtitle")}
                   />
                 </CardHeader>
-                <CardBody></CardBody>
+                <CardBody>
+                  <Row>
+                    <Col sm="6">
+                      <InputGroup
+                        htmlFor={"coordinates[0]"}
+                        label={t("form.coordinates.latitude")}
+                        error={form.errors.latitude}
+                      >
+                        <Input
+                          id="coordinates[0]"
+                          name="coordinates[0]"
+                          type="text"
+                          className="form-control"
+                          placeholder={t("form.coordinates.latitude")}
+                          pattern="^(-?\d{1,2}(?:\.\d{1,6})?|[-+]?[1-8]?\d(?:\.\d{1,6})?|[-+]?90(?:\.0{1,6})?)$"
+                          onChange={form.handleChange}
+                          value={form.values.coordinates[0]}
+                          invalid={!!form.errors.coordinates}
+                        />
+                      </InputGroup>
+                    </Col>
+                    <Col sm="6">
+                      <InputGroup
+                        htmlFor={"coordinates[1]"}
+                        label={t("form.coordinates.longitude")}
+                        error={form.errors.longitude}
+                      >
+                        <Input
+                          id="coordinates[1]"
+                          name="coordinates[1]"
+                          type="text"
+                          className="form-control"
+                          placeholder={t("form.coordinates.longitude")}
+                          pattern="^(-?\d{1,3}(?:\.\d{1,6})?|[-+]?1[0-7]\d(?:\.\d{1,6})?|[-+]180(?:\.0{1,6})?)$"
+                          onChange={form.handleChange}
+                          value={form.values.coordinates[1]}
+                          invalid={!!form.errors.coordinates}
+                        />
+                      </InputGroup>
+                    </Col>
+                  </Row>
+                </CardBody>
               </Card>
             </Col>
             <Col xs="12">
-              <Card>
+              <Card className="r-card">
                 <CardHeader>
                   <CardHeadContent
                     title={t("form.translations.title")}
@@ -208,7 +285,7 @@ const PlaceCreateView = () => {
               </Card>
             </Col>
             <Col xs="12">
-              <Card>
+              <Card className="r-card">
                 <CardHeader>
                   <CardHeadContent
                     title={t("form.image.title")}
@@ -231,7 +308,7 @@ const PlaceCreateView = () => {
               </Card>
             </Col>
             <Col xs="12">
-              <Card>
+              <Card className="r-card">
                 <CardHeader>
                   <CardHeadContent
                     title={t("form.detail.title")}
