848f2c6f47161dcd43cf762ebecdeeb12c864182
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-09-30 00:02:09 +0300

    feature/category: add meta fields to form

--- src/domains/root/subdomains/category/views/CategoryCreateView.jsx
+++ src/domains/root/subdomains/category/views/CategoryCreateView.jsx
@@ -1,21 +1,66 @@
 import CardHeadContent from "@/components/Kit/CardHeadContent";
+import InputGroup from "@/components/Kit/InputGroup";
 import RBreadcrumb from "@/components/Kit/RBreadcrumb";
+import { Config } from "@/config/config";
 import { Roles } from "@/config/roles";
 import { useFormik } from "formik";
+import { Fragment } from "react";
 import { useTranslation } from "react-i18next";
-import { Button, Card, CardBody, CardHeader, Col, Form, Row } from "reactstrap";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  Form,
+  Input,
+  Row,
+} from "reactstrap";
 import Spin from "sspin";
 import { v4 as uuidv4 } from "uuid";
 import PageContentLayout from "~domains/root/layout/PageContentLayout";
 import ClaimGuardLayout from "~subdomains/account/layout/ClaimGuardLayout";
 import CategoryInputGroupForm from "../components/CategoryInputGroupForm";
-import CategoryInputPreview from "../components/CategoryInputPreview";
 
 const CategoryCreateView = () => {
   const { t } = useTranslation("categories");
 
   const form = useFormik({
     initialValues: {
+      images: [
+        {
+          url: "",
+          order: 0,
+        },
+      ],
+      meta: {
+        tr: {
+          name: "",
+          description: "",
+          title: "",
+          markdownURL: "",
+          seo: {
+            title: "",
+            description: "",
+            keywords: "",
+            canonical: "",
+            extra: [],
+          },
+        },
+        en: {
+          name: "",
+          description: "",
+          title: "",
+          markdownURL: "",
+          seo: {
+            title: "",
+            description: "",
+            keywords: "",
+            canonical: "",
+            extra: [],
+          },
+        },
+      },
       inputGroups: [
         {
           uuid: uuidv4(),
@@ -33,6 +78,40 @@ const CategoryCreateView = () => {
         },
       ],
       inputs: [],
+      rules: [
+        {
+          uuid: uuidv4(),
+          translations: {
+            tr: {
+              name: "",
+              description: "",
+            },
+            en: {
+              name: "",
+              description: "",
+            },
+          },
+          strictLevel: 0, // max 10
+        },
+      ],
+      alerts: [
+        {
+          uuid: uuidv4(),
+          translations: {
+            tr: {
+              name: "",
+              description: "",
+            },
+            en: {
+              name: "",
+              description: "",
+            },
+          },
+          type: "info", // info, warning, error
+        },
+      ],
+      validators: ["required"],
+      order: 0,
     },
   });
 
@@ -124,6 +203,178 @@ const CategoryCreateView = () => {
                 <Card className="r-card">
                   <CardHeader>
                     <CardHeadContent
+                      title={t("form.meta.title")}
+                      subtitle={t("form.meta.subtitle")}
+                    />
+                  </CardHeader>
+                  <CardBody>
+                    <Row>
+                      {Config.langs.map((lang, index) => (
+                        <Fragment key={lang + "trans" + index}>
+                          <Col xs="12">
+                            <h5>{t(`translate.${lang}`)}</h5>
+                          </Col>
+                          <Col xs={12}>
+                            <InputGroup
+                              htmlFor={`meta[${lang}].name`}
+                              label={t("form.meta.name.label")}
+                              error={
+                                form.errors.meta && form.errors.meta[lang]?.name
+                              }
+                            >
+                              <Input
+                                id={`meta[${lang}].name`}
+                                name={`meta[${lang}].name`}
+                                type="text"
+                                className="form-control"
+                                placeholder={t("form.meta.name.placeholder")}
+                                onChange={form.handleChange}
+                                value={form.values.meta[lang].name}
+                                invalid={
+                                  !!form.errors.meta &&
+                                  !!form.errors.meta[lang]?.name
+                                }
+                              />
+                            </InputGroup>
+                          </Col>
+                          <Col xs={12}>
+                            <InputGroup
+                              htmlFor={`meta[${lang}].title`}
+                              label={t("form.meta.title.label")}
+                              error={
+                                form.errors.meta &&
+                                form.errors.meta[lang]?.title
+                              }
+                            >
+                              <Input
+                                id={`meta[${lang}].title`}
+                                name={`meta[${lang}].title`}
+                                type="text"
+                                className="form-control"
+                                placeholder={t("form.meta.title.placeholder")}
+                                onChange={form.handleChange}
+                                value={form.values.meta[lang].title}
+                                invalid={
+                                  !!form.errors.meta &&
+                                  !!form.errors.meta[lang]?.title
+                                }
+                              />
+                            </InputGroup>
+                          </Col>
+                          <Col xs={12}>
+                            <InputGroup
+                              htmlFor={`meta[${lang}].description`}
+                              label={t("form.meta.description.label")}
+                              error={
+                                form.errors.meta &&
+                                form.errors.meta[lang]?.description
+                              }
+                            >
+                              <Input
+                                id={`meta[${lang}].description`}
+                                name={`meta[${lang}].description`}
+                                type="textarea"
+                                className="form-control"
+                                placeholder={t(
+                                  "form.meta.description.placeholder"
+                                )}
+                                onChange={form.handleChange}
+                                value={form.values.meta[lang].description}
+                                invalid={
+                                  !!form.errors.meta &&
+                                  !!form.errors.meta[lang]?.description
+                                }
+                              />
+                            </InputGroup>
+                          </Col>
+                          <Col xs={12} sm={6}>
+                            <InputGroup
+                              htmlFor={`meta[${lang}].seo.title`}
+                              label={t("form.meta.seo.title.label")}
+                              error={
+                                form.errors.meta &&
+                                form.errors.meta[lang]?.seo.title
+                              }
+                            >
+                              <Input
+                                id={`meta[${lang}].seo.title`}
+                                name={`meta[${lang}].seo.title`}
+                                type="text"
+                                className="form-control"
+                                placeholder={t(
+                                  "form.meta.seo.title.placeholder"
+                                )}
+                                onChange={form.handleChange}
+                                value={form.values.meta[lang].seo.title}
+                                invalid={
+                                  !!form.errors.meta &&
+                                  !!form.errors.meta[lang]?.seo.title
+                                }
+                              />
+                            </InputGroup>
+                          </Col>
+                          <Col xs={12} sm={6}>
+                            <InputGroup
+                              htmlFor={`meta[${lang}].seo.keywords`}
+                              label={t("form.meta.seo.keywords.label")}
+                              error={
+                                form.errors.meta &&
+                                form.errors.meta[lang]?.seo.keywords
+                              }
+                            >
+                              <Input
+                                id={`meta[${lang}].seo.keywords`}
+                                name={`meta[${lang}].seo.keywords`}
+                                type="text"
+                                className="form-control"
+                                placeholder={t(
+                                  "form.meta.seo.keywords.placeholder"
+                                )}
+                                onChange={form.handleChange}
+                                value={form.values.meta[lang].seo.keywords}
+                                invalid={
+                                  !!form.errors.meta &&
+                                  !!form.errors.meta[lang]?.seo.keywords
+                                }
+                              />
+                            </InputGroup>
+                          </Col>
+                          <Col xs={12}>
+                            <InputGroup
+                              htmlFor={`meta[${lang}].seo.description`}
+                              label={t("form.meta.seo.description.label")}
+                              error={
+                                form.errors.meta &&
+                                form.errors.meta[lang]?.seo.description
+                              }
+                            >
+                              <Input
+                                id={`meta[${lang}].seo.description`}
+                                name={`meta[${lang}].seo.description`}
+                                type="text"
+                                className="form-control"
+                                placeholder={t(
+                                  "form.meta.seo.description.placeholder"
+                                )}
+                                onChange={form.handleChange}
+                                value={form.values.meta[lang].seo.description}
+                                invalid={
+                                  !!form.errors.meta &&
+                                  !!form.errors.meta[lang]?.seo.description
+                                }
+                              />
+                            </InputGroup>
+                          </Col>
+                        </Fragment>
+                      ))}
+                    </Row>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col xs={12}>
+                <Card className="r-card">
+                  <CardHeader>
+                    <CardHeadContent
                       title={t("form.inputs.title")}
                       subtitle={t("form.inputs.subtitle")}
                     />
@@ -161,6 +412,11 @@ const CategoryCreateView = () => {
                     ))}
                   </CardBody>
                 </Card>
+              </Col>
+              <Col xs="12" className="mb-4">
+                <Button color="primary" type="submit">
+                  {t("form.submit")}
+                </Button>
               </Col>
             </Row>
           </Form>
