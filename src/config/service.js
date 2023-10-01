c1d7476e1132fb01d0b145a5d2608247f51b526b
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-10-01 20:30:01 +0300

    feature/category: update the service config

--- src/config/service.js
+++ src/config/service.js
@@ -4,6 +4,7 @@ export const Services = {
   Upload: "upload",
   Admin: "admin",
   Place: "place",
+  Category: "category",
 };
 
 export const ApiUrls = {
@@ -12,6 +13,7 @@ export const ApiUrls = {
   [Services.Account]: import.meta.env.VITE_API_ACCOUNT_SRV_URL,
   [Services.Upload]: import.meta.env.VITE_API_UPLOAD_SRV_URL,
   [Services.Place]: import.meta.env.VITE_API_PLACE_SRV_URL,
+  [Services.Category]: import.meta.env.VITE_API_CATEGORY_SRV_URL,
 };
 
 export const apiUrl = (service, path) => `${ApiUrls[service]}${path}`;
