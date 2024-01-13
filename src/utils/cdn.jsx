2bfae07199e04354fdc152c7b3450b54a4be41f0
9ssi7 <info@ssibrahimbas.com>
2024-01-14 00:37:08 +0300

    feature/listing-module: complete the list view

--- src/utils/cdn.jsx
+++ src/utils/cdn.jsx
@@ -1,1 +1,2 @@
-export const makeUserAvatar = (user) => `https://avatar.turistikrota.com/@${user}`
+export const makeUserAvatar = (user) => `https://avatar.turistikrota.com/@${user}.png`
+export const makeBusinessAvatar = (business) => `https://avatar.turistikrota.com/~${business}.png`
