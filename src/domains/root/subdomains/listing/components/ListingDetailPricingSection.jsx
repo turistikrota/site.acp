5d8512418a7412d18c517ce6ff01371094ff6030
9ssi7 <info@ssibrahimbas.com>
2024-01-15 22:19:18 +0300

    feature/listing-module: add pricing section

--- src/domains/root/subdomains/listing/components/ListingDetailPricingSection.jsx
+++ src/domains/root/subdomains/listing/components/ListingDetailPricingSection.jsx
@@ -1,5 +1,24 @@
+import KeyValue from "@/components/Kit/key-value/KeyValue";
+import { useLocalizedCurrencyFormatter } from "@/hooks/intl";
+import { useDayJS } from "@/utils/dayjs";
+import { useTranslation } from "react-i18next";
+import { Col, Row } from "reactstrap";
+
 const ListingDetailPricingSection = ({prices}) => {
-    return <></>
+    const { t } = useTranslation("listing");
+    const dayjs = useDayJS()
+    const formatter =useLocalizedCurrencyFormatter()
+    return <Row className="mt-4 gap-y-4">
+        <Col xs={12}>
+        <h4 className="text-xl font-semibold">{t('details.pricing.title')}</h4>
+        </Col>
+        {prices.map((price, idx) => <Col key={idx} xs={12} md={6}>
+                <KeyValue label={t('details.pricing.range', {
+                    start: dayjs(price.startDate).format('DD MMMM YYYY'),
+                    end: dayjs(price.endDate).format('DD MMMM YYYY')
+                })} value={formatter.format(price.price)} />
+            </Col>)}
+    </Row>
 }
 
 export default ListingDetailPricingSection
