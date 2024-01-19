c29a372499d12dcb2de1d36be92b4b5f01277c97
9ssi7 <info@ssibrahimbas.com>
2024-01-19 21:50:21 +0300

    feature/help: fix article edit view

--- src/utils/i18n.jsx
+++ src/utils/i18n.jsx
@@ -1,4 +1,5 @@
 export const getTranslation = (obj, locale, def = undefined) => {
+    if(!obj) return def;
     if(obj[locale]) return obj[locale];
     if(obj['en']) return obj['en'];
     if(obj['tr']) return obj['tr'];
