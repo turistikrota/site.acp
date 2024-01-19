6f3806a900843dda6651f999c57bc8fc9c5d6ba4
9ssi7 <info@ssibrahimbas.com>
2024-01-19 21:32:53 +0300

    feature/help: add article craete view

--- src/domains/root/subdomains/help/views/HelpArticleCreateView.jsx
+++ src/domains/root/subdomains/help/views/HelpArticleCreateView.jsx
@@ -1,115 +1,180 @@
 import CardHeadContent from "@/components/Kit/CardHeadContent";
 import InputGroup from "@/components/Kit/InputGroup";
-import MarkdownEditor, { uploadMdContent } from "@/components/Kit/MarkdownContent";
+import MarkdownEditor, {
+  uploadMdContent,
+} from "@/components/Kit/MarkdownContent";
 import RBreadcrumb from "@/components/Kit/RBreadcrumb";
 import { Config } from "@/config/config";
 import { Roles } from "@/config/roles";
 import { Services, apiUrl } from "@/config/service";
 import PageContentLayout from "@/domains/root/layout/PageContentLayout";
+import { useQuery } from "@/hooks/query";
 import { httpClient } from "@/http/client";
 import { useAlert } from "@/utils/alert";
 import { handleApiError } from "@/utils/api-error";
+import { makeCustomSelect } from "@/utils/customSelect";
+import { getTranslation } from "@/utils/i18n";
 import { useMeta } from "@/utils/site";
 import { useFormik } from "formik";
 import { Fragment, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Form, useNavigate } from "react-router-dom";
-import { Button, Card, CardBody, CardHeader, Col, Input, Row } from "reactstrap";
+import Select from "react-select";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  Input,
+  Row,
+} from "reactstrap";
 import Spin from "sspin/dist/esm/Spin";
 import ClaimGuardLayout from "../../account/layout/ClaimGuardLayout";
 
 const HelpArticleCreateView = () => {
-    const [loading, setLoading] = useState(false);
-    const { t } = useTranslation("help");
-    const [trMarkdown, setTrMarkdown] = useState("");
-    const [enMarkdown, setEnMarkdown] = useState("");
-    const navigate = useNavigate();
-    const alert = useAlert();
-    useMeta(t("faq.create.title"));
+  const [loading, setLoading] = useState(false);
+  const { t, i18n } = useTranslation("help");
+  const [trMarkdown, setTrMarkdown] = useState("");
+  const [enMarkdown, setEnMarkdown] = useState("");
+  const navigate = useNavigate();
+  const alert = useAlert();
+  const { data: categories, isLoading : categoryLoading } = useQuery(
+    apiUrl(
+      Services.Help,
+      `/admin/category`
+    )
+  );
+  useMeta(t("article.create.title"));
 
-    const form = useFormik({
-        initialValues: {
-            meta: {
-                tr: {
-                    title: '',
-                    description: '',
-                    keywords: ''
-                },
-                en: {title: '',
-                description: '',
-                keywords: ''
-            }
-            }
+  const form = useFormik({
+    initialValues: {
+      categoryId: "",
+      meta: {
+        tr: {
+          title: "",
+          keywords: "",
         },
-        validateOnChange: false,
-        validateOnBlur: false,
-        onSubmit: async(values) => {
-            if (
-                !(await alert.check({
-                  text: t("faq.create.check"),
-                }))
-              )
-                return;
-              setLoading(true);
-            const res =await httpClient.post(apiUrl(Services.Help, `/admin/faq`), values).catch(handleApiError(alert, form))
-            setLoading(false);
-            if (!res || ![200, 201].includes(res.status)) return;
-            const uuid = res.data.uuid;
-            const [enContent, trContent] = await Promise.all([
-              uploadMdContent(enMarkdown, Config.cdn.apps.help, {
-                randomName: false,
-                fileName: uuid + ".en",
-              }),
-              uploadMdContent(trMarkdown, Config.cdn.apps.help, {
-                randomName: false,
-                fileName: uuid + ".tr",
-              }),
-            ]);
-            setLoading(false);
-            if (!enContent || !trContent) {
-              return alert.error({
-                text: t("upload.failed"),
-              });
-            }
-            navigate("/faqs");
-        }
-    })
+        en: { title: "", keywords: "" },
+      },
+    },
+    validateOnChange: false,
+    validateOnBlur: false,
+    onSubmit: async (values) => {
+      if (
+        !(await alert.check({
+          text: t("article.create.check"),
+        }))
+      )
+        return;
+      setLoading(true);
+      const res = await httpClient
+        .post(apiUrl(Services.Help, `/admin/article`), values)
+        .catch(handleApiError(alert, form));
+      setLoading(false);
+      if (!res || ![200, 201].includes(res.status)) return;
+      const uuid = res.data.uuid;
+      const [enContent, trContent] = await Promise.all([
+        uploadMdContent(enMarkdown, Config.cdn.apps.help, {
+          randomName: false,
+          fileName: uuid + ".en",
+        }),
+        uploadMdContent(trMarkdown, Config.cdn.apps.help, {
+          randomName: false,
+          fileName: uuid + ".tr",
+        }),
+      ]);
+      setLoading(false);
+      if (!enContent || !trContent) {
+        return alert.error({
+          text: t("upload.failed"),
+        });
+      }
+      navigate("/help");
+    },
+  });
 
-    const onSubmit = (e) => {
-        e.preventDefault()
-        form.submitForm();
-    }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    form.submitForm();
+  };
 
-    return <ClaimGuardLayout pageName={t("faq.create.title")}
-    roles={[Roles.admin, Roles.Help.Super, Roles.Help.Faq.super, Roles.Help.Faq.create]}>
-        <PageContentLayout>
-        <RBreadcrumb title={t("faq.create.title")}>
-          <RBreadcrumb.Item to="/faqs">
-            {t("faq.list.title")}
+  return (
+    <ClaimGuardLayout
+      pageName={t("article.create.title")}
+      roles={[
+        Roles.admin,
+        Roles.Help.Super,
+        Roles.Help.Article.super,
+        Roles.Help.Article.create,
+      ]}
+    >
+      <PageContentLayout>
+        <RBreadcrumb title={t("article.create.title")}>
+          <RBreadcrumb.Item to="/help">
+            {t("article.list.title")}
           </RBreadcrumb.Item>
-          <RBreadcrumb.Current>{t("faq.create.title")}</RBreadcrumb.Current>
+          <RBreadcrumb.Current>{t("article.create.title")}</RBreadcrumb.Current>
         </RBreadcrumb>
-        <Spin loading={loading}>
-            <Form onSubmit={onSubmit}>
-                <Row>
-                    <Col xs={12}>
-                        <Card className="r-card">
-                        <CardHeader>
-                      <CardHeadContent
-                        title={t("faq.form.meta.section.title")}
-                        subtitle={t("faq.form.meta.section.subtitle")}
-                      />
-                    </CardHeader>
-                    <CardBody>
-                      <Row>
-                        {Config.langs.map((lang, index) => <Fragment key={`${lang}trans${index}`}>
-                            <Col xs={12}>
-                                <h5>{t(`translate.${lang}`)}</h5>
-                            </Col>
-                            <Col xs={12}>
+        <Spin loading={loading || categoryLoading}>
+          <Form onSubmit={onSubmit}>
+            <Row>
+              <Col xs={12}>
+                <Card className="r-card">
+                <CardHeader>
+                    <CardHeadContent
+                      title={t("article.form.category.title")}
+                      subtitle={t("article.form.category.subtitle")}
+                    />
+                  </CardHeader>
+                  <CardBody>
+                  <InputGroup
+                          htmlFor="type"
+                          label={t("article.form.category.label")}
+                          error={form.errors.type}
+                        >
+                          <Select
+                            classNamePrefix="select2-selection"
+                            placeholder={t("article.form.category.placeholder")}
+                            title={t("article.form.category.selectTitle")}
+                            options={(categories || []).map((d) => ({
+                              value: d.uuid,
+                              label: getTranslation(d.meta, i18n.language, {title:''}).title,
+                            }))}
+                            id="categoryId"
+                            name="categoryId"
+                            className={form.errors.categoryId ? "is-invalid" : ""}
+                            invalid={!!form.errors.categoryId}
+                            aria-invalid={!!form.errors.categoryId}
+                            aria-errormessage={form.errors.categoryId}
+                            onChange={(e) => {
+                              form.setFieldValue("categoryId", e.value);
+                            }}
+                            theme={makeCustomSelect}
+                          />
+                        </InputGroup>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col xs={12}>
+                <Card className="r-card">
+                  <CardHeader>
+                    <CardHeadContent
+                      title={t("article.form.meta.section.title")}
+                      subtitle={t("article.form.meta.section.subtitle")}
+                    />
+                  </CardHeader>
+                  <CardBody>
+                    <Row>
+                      {Config.langs.map((lang, index) => (
+                        <Fragment key={`${lang}trans${index}`}>
+                          <Col xs={12}>
+                            <h5>{t(`translate.${lang}`)}</h5>
+                          </Col>
+                          <Col xs={12}>
                             <InputGroup
                               htmlFor={`meta[${lang}].title`}
-                              label={t("faq.form.meta.title.label")}
+                              label={t("article.form.meta.title.label")}
                               error={
                                 form.errors.meta &&
                                 form.errors.meta[lang]?.title
@@ -120,7 +185,9 @@ const HelpArticleCreateView = () => {
                                 name={`meta[${lang}].title`}
                                 type="text"
                                 className="form-control"
-                                placeholder={t("faq.form.meta.title.placeholder")}
+                                placeholder={t(
+                                  "article.form.meta.title.placeholder"
+                                )}
                                 onChange={form.handleChange}
                                 value={form.values.meta[lang].title}
                                 invalid={
@@ -129,37 +196,11 @@ const HelpArticleCreateView = () => {
                                 }
                               />
                             </InputGroup>
-                            </Col>
-                            <Col xs={12}>
-                            <InputGroup
-                              htmlFor={`meta[${lang}].description`}
-                              label={t("faq.form.meta.description.label")}
-                              error={
-                                form.errors.meta &&
-                                form.errors.meta[lang]?.description
-                              }
-                            >
-                              <Input
-                                id={`meta[${lang}].description`}
-                                name={`meta[${lang}].description`}
-                                type="textarea"
-                                className="form-control"
-                                placeholder={t(
-                                  "faq.form.meta.description.placeholder"
-                                )}
-                                onChange={form.handleChange}
-                                value={form.values.meta[lang].description}
-                                invalid={
-                                  !!form.errors.meta &&
-                                  !!form.errors.meta[lang]?.description
-                                }
-                              />
-                            </InputGroup>
                           </Col>
                           <Col xs={12}>
                             <InputGroup
                               htmlFor={`meta[${lang}].keywords`}
-                              label={t("faq.form.meta.keywords.label")}
+                              label={t("article.form.meta.keywords.label")}
                               error={
                                 form.errors.meta &&
                                 form.errors.meta[lang]?.keywords
@@ -170,7 +211,9 @@ const HelpArticleCreateView = () => {
                                 name={`meta[${lang}].keywords`}
                                 type="text"
                                 className="form-control"
-                                placeholder={t("faq.form.meta.keywords.placeholder")}
+                                placeholder={t(
+                                  "article.form.meta.keywords.placeholder"
+                                )}
                                 onChange={form.handleChange}
                                 value={form.values.meta[lang].keywords}
                                 invalid={
@@ -179,18 +222,19 @@ const HelpArticleCreateView = () => {
                                 }
                               />
                             </InputGroup>
-                            </Col>
-                        </Fragment>)}
-                      </Row>
-                    </CardBody>
-                        </Card>
-                    </Col>
-                    <Col xs="12">
+                          </Col>
+                        </Fragment>
+                      ))}
+                    </Row>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col xs="12">
                 <Card className="r-card">
                   <CardHeader>
                     <CardHeadContent
-                      title={t("faq.form.detail.title")}
-                      subtitle={t("faq.form.detail.subtitle")}
+                      title={t("article.form.detail.title")}
+                      subtitle={t("article.form.detail.subtitle")}
                     />
                   </CardHeader>
                   <CardBody>
@@ -218,14 +262,12 @@ const HelpArticleCreateView = () => {
                   {t("faq.form.submit")}
                 </Button>
               </Col>
-                </Row>
-            </Form>
+            </Row>
+          </Form>
         </Spin>
-        </PageContentLayout>
+      </PageContentLayout>
     </ClaimGuardLayout>
-}
-
-export {
-    HelpArticleCreateView as Component
+  );
 };
 
+export { HelpArticleCreateView as Component };
